e720d337707334c34dc81c6055d662e106b80899
Filipe S. de Queiroz <filipesantiago.qrz@gmail.com>
2017-05-04 12:56:39 -0300

    [Dashboard] Inserting js script path

--- web/index.php
+++ web/index.php
@@ -242,7 +242,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script>window.jQuery || document.write('<script src="../vendor/twbs/bootstrap/docs/assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
     <script src="../vendor/twbs/bootstrap/docs/assets/js/vendor/holder.min.js"></script>
