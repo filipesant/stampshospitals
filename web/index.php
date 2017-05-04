b3fd4b2889ecf2f2252a5818559f13c73625c00d
Filipe S. de Queiroz <filipesantiago.qrz@gmail.com>
2017-05-04 14:20:20 -0300

    [Dashboard] Testing

--- web/index.php
+++ web/index.php
@@ -245,11 +245,11 @@ session_start();
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../twbs/bootstrap/docs/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <!--<script>window.jQuery || document.write('<script src="../twbs/bootstrap/docs/assets/js/vendor/jquery.min.js"><\/script>')</script> -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap‌​.min.js"></script>
     <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="../twbs/bootstrap/docs/assets/js/vendor/holder.min.js"></script>
+    <!-- <script src="../twbs/bootstrap/docs/assets/js/vendor/holder.min.js"></script> -->
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../twbs/bootstrap/docs/assets/js/ie10-viewport-bug-workaround.js"></script>
+    <!--<script src="../twbs/bootstrap/docs/assets/js/ie10-viewport-bug-workaround.js"></script>->
   </body>
 </html>
